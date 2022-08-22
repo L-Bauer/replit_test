7e049768c842e1a80f13904ca8ddebf5e012aafe
L_Bauer <85202822+L-Bauer@users.noreply.github.com>
2022-08-22 20:59:16 +0000

    Added debugger

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
-puts "Hello, World!"
+puts 'Hello, World!'
+
+require 'pry-byebug'
+
+def isogram?(string)
+  original_length = string.length
+  string_array = string.downcase.split
+
+  binding.pry
+
+  unique_length = string_array.uniq.length
+  original_length == unique_length
+end
+
+isogram?('Odin')
